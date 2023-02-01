662710f8513716c111359bb7c46fc400b46c114e
Hugh Shanahan <Hugh.Shanahan@rhul.ac.uk>
2023-02-01 21:44:01 +0000

    Updated cp3; corrected diffusion algo in notebook

--- tester/cp3.py
+++ tester/cp3.py
@@ -6,46 +6,28 @@ test = {
             'cases': [
                 {
                     'code': r"""
-                    >>> # It seems cmpInverses is undefined. Have you defined it correctly?
-                    >>> 'cmpInverses' in dir()
+                    >>> # It seems that runDiffusion is undefined. Have you defined it correctly?
+                    >>> 'runDiffusion' in dir()
                     True
                     """
                 },
                 {
                     'code': r"""
-                    >>> # Your cmpInverses function isn't returning the correct result.
-                    >>> # The value you have computed manually doesn't match the actual inverse.
-                    >>> # Check that you're computing this correctly. 
-                    >>> # You should use np.matmul to multiply the matrices.
-                    >>> cmpInverses(np.load('tester/res/cp3.npy')) == np.linalg.inv(test)
+                    >>> # It seems that runDiffusion is not returning a numpy vector. Have you defined it correctly?
+                    >>> # Remember if the adjacency matrix is an NxN matrix then it should return an N-dim column vector.
+                    >>> runDiffusion(np.array([[0,1],[1,0]]),1e-5).shape = (2,)
+                    True
+                    """
+                },                
+                {
+                    'code': r"""
+                    >>> # Your version of runDiffusion isn't giving the correct output.
+                    >>> # Please have another look at the algorithm. 
+                    >>> finalAnswercp3 = np.array([0.33217593 0.33287037 0.16689815 0.11064815 0.05740741])
+                    >>> np.allclose(finalAnswercp3,runDiffusion(np.array([[0,1,0,0,0], [1,0,1,0,0], [1,0,0,1,1], [1,0,0,0,0], [0,0,0,1,0]],0.01),atol=10**-2, rtol=0) 
                     True
                     """
                 },
-                {
-                    'code': r"""
-                    >>> # I can't find your sqDiffMatrices function from lab 2.
-                    >>> # Have you copied it across?
-                    >>> # Make sure you keep the function name the same!
-                    >>> 'sqDiffMatrices' in dir()
-                    True
-                    """
-                },
-                {
-                    'code': r"""
-                    >>> # It seems cmpInverses is undefined. Have you defined it correctly?
-                    >>> 'sqDiff' in dir()
-                    True
-                    """
-                },
-                {
-                    'code': r"""
-                    >>> # Hmm...your sqDiff is incorrect.
-                    >>> # Have you hard-coded something you shouldn't have?
-                    >>> import math
-                    >>> math.isclose(sqDiff, 999410.2525124999, rel_tol=1e-5, abs_tol=0)
-                    True
-                    """
-                }
             ],
             'scored': True,
             'type': 'doctest'
