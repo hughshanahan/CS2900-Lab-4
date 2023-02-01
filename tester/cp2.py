5827a3aa9e609b00b1f845314989f022bec0e6b5
Hugh Shanahan <Hugh.Shanahan@rhul.ac.uk>
2023-02-01 17:09:54 +0000

    updated tester function for checkpoint 2

--- tester/cp2.py
+++ tester/cp2.py
@@ -1,68 +1,39 @@
 test = {
-    'name': 'checkpoint-2',
+    'name': 'checkpoint-1',
     'points': 1,
     'suites': [
         {
             'cases': [
                 {
                     'code': r"""
-                    >>> # It seems cmpCondNums is undefined. Have you defined it correctly?
-                    >>> 'cmpCondNums' in dir()
+                    >>> # It seems that A1 is undefined. Have you defined it correctly?
+                    >>> 'A1' in dir()
                     True
                     """
                 },
                 {
                     'code': r"""
-                    >>> # Your cmpCondNums function isn't returning the correct type.
-                    >>> # We're expecting a tuple!
-                    >>> cond_nums = cmpCondNums(test_array)
-                    >>> type(cond_nums).__name__ == 'tuple'
+                    >>> # It seems that D1hand is undefined. Have you defined it correctly?
+                    >>> 'D1hand' in dir()
+                    True
+                    """
+                },                
+                {
+                    'code': r"""
+                    >>> # It seems the dimensons of D1hand are incorrect.
+                    >>> # Are you checking if the dimensions of D1hand are those of A1? 
+                    >>> D1hand.shape == (5,5) 
                     True
                     """
                 },
                 {
                     'code': r"""
-                    >>> # Your cmpCondNums function isn't returning all the values.
-                    >>> # We are looking for 3 values; BOTH calculations and the difference between them.
-                    >>> cond_nums = cmpCondNums(test_array)
-                    >>> len(cond_nums) == 3
+                    >>> # The diffusion matrix of A1 computed using compDiffusion does not match with D1hand. 
+                    >>> # Have you checked your calculation of D1hand?
+                    >>> np.allclose(compDiffusion(A1),D1hand,rtol=0.01) 
                     True
                     """
                 },
-                {
-                    'code': r"""
-                    >>> # Your first calculation is incorrect.
-                    >>> # This calculation should use the np.linalg.cond function.
-                    >>> import math
-                    >>> test_array = np.load('tester/res/cp2.npy')
-                    >>> cond_nums = cmpCondNums(test_array)
-                    >>> math.isclose(cond_nums[0], np.linalg.cond(test_array), rel_tol=1e+7, abs_tol=0)
-                    True
-                    """
-                },
-                {
-                    'code': r"""
-                    >>> # Your second calculation is incorrect.
-                    >>> # Check that you're getting computing the min and max elements correctly.
-                    >>> # You need to divide them to find the condition number.
-                    >>> import math
-                    >>> test_array = np.load('tester/res/cp2.npy')
-                    >>> cond_nums = cmpCondNums(test_array)
-                    >>> math.isclose(cond_nums[1], np.linalg.cond(test_array), rel_tol=1e+7, abs_tol=0)
-                    True
-                    """
-                },
-                {
-                    'code': r"""
-                    >>> # The absolute difference is incorrect. It should simply be zero.
-                    >>> import math
-                    >>> test_array = np.load('tester/res/cp2.npy')
-                    >>> cond_nums = cmpCondNums(test_array)
-                    >>> math.isclose(cond_nums[2], 0.0, rel_tol=1e-5, abs_tol=0)
-                    True
-                    """
-                },
-                
             ],
             'scored': True,
             'type': 'doctest'
