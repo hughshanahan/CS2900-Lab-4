4ea2261e98f610893b587d1d257faff351fd0ba3
Hugh Shanahan <Hugh.Shanahan@rhul.ac.uk>
2023-03-02 22:48:17 +0000

    Updates to tester code for second half of lab 4

--- tester/cp7.py
+++ tester/cp7.py
@@ -17,32 +17,7 @@ test = {
                     >>> # The value you have computed manually doesn't match the actual inverse.
                     >>> # Check that you're computing this correctly. 
                     >>> # You should use np.matmul to multiply the matrices.
-                    >>> cmpInverses(np.load('tester/res/cp7.npy')) == np.linalg.inv(test)
-                    True
-                    """
-                },
-                {
-                    'code': r"""
-                    >>> # I can't find your sqDiffMatrices function from lab 2.
-                    >>> # Have you copied it across?
-                    >>> # Make sure you keep the function name the same!
-                    >>> 'sqDiffMatrices' in dir()
-                    True
-                    """
-                },
-                {
-                    'code': r"""
-                    >>> # It seems cmpInverses is undefined. Have you defined it correctly?
-                    >>> 'sqDiff' in dir()
-                    True
-                    """
-                },
-                {
-                    'code': r"""
-                    >>> # Hmm...your sqDiff is incorrect.
-                    >>> # Have you hard-coded something you shouldn't have?
-                    >>> import math
-                    >>> math.isclose(sqDiff, 999410.2525124999, rel_tol=1e-5, abs_tol=0)
+                    >>> np.allclose(cmpInverses(A1), np.linalg.inv(A1))
                     True
                     """
                 }
--- tester/cp8.py
+++ tester/cp8.py
@@ -15,7 +15,7 @@ test = {
                     'code': r"""
                     >>> # Your doSVDFit function isn't returning the correct type.
                     >>> # We're expecting a tuple, not a NumPy array!
-                    >>> p = doSVDFit(x, y)
+                    >>> p = doSVDFit(np.array([0, 1, 2, 3]), np.array([-1, 0.2, 0.9, 2.1]))
                     >>> type(p).__name__ == 'tuple'
                     True
                     """
@@ -26,7 +26,9 @@ test = {
                     >>> # Refer to the formula given and ensure you're calculating m and c
                     >>> # using the np.linalg.pinv function.
                     >>> x, y = np.load('tester/res/cp8_x.npy'), np.load('tester/res/cp8_y.npy')
-                    >>> doSVDFit(x, y) == (1, -0.95)
+                    >>> import math
+                    >>> pr = doSVDFit(x, y) 
+                    >>> math.isclose(pr[0],1.0) and math.isclose(pr[1],-0.95)
                     True
                     """
                 }
